30e51af4265a509061957299115d736511248a11
Tнαιѕ Cσηтяєяαѕ <thaisc98@hotmail.es>
2019-06-07 10:45:06 -0500

    Creacion de Proyecto

---
+++ Vector/Vector/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+	class Program
+	{
+		private int[] sueldos;
+
+		public void Cargar()
+		{
+			sueldos = new int[5];
+			for (int x = 0; x < 5; x++)
+			{
+				Console.Write("Ingrese valor de la componente:");
+				String linea;
+				linea = Console.ReadLine();
+				sueldos[x] = int.Parse(linea);
+			}
+		}
+		public void Imprimir()
+		{
+			for (int x = 0; x < 5; x++)
+			{
+				Console.WriteLine(sueldos[x]);
+			}
+			Console.ReadKey();
+		}
+
+
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			p.Cargar();
+			p.Imprimir();
+		}
+		
+	}
+}
